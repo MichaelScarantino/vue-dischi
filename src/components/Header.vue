25d8cbcc853874881f3be76261dd32ca7ce09ac7
MichaelScarantino <scarantino.michael@outlook.it>
2021-12-22 15:19:32 +0100

    Done Header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <div class="header-logo">
+            <img src="../assets/img/logo.png" alt="Logo Spotify">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/variables";
+header{
+    width: 100%;
+    height: 80px;
+    background-color: $brand_primary_color;
+    line-height: 80px;
+
+    .header-logo{
+        width: 50px;
+        margin-left: 20px;
+    }
+}
+
+</style>
